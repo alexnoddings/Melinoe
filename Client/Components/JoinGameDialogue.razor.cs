95f8f0f7107ff0a8ed192916d4cee74248945c7f
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-02-20 14:47:48 +0000

    Update game join dialogue.

--- Client/Components/JoinGameDialogue.razor.cs
+++ Client/Components/JoinGameDialogue.razor.cs
@@ -1,4 +1,9 @@
-﻿using Fluxor;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Blazorise;
+using Fluxor;
 using Melinoe.Client.State.JoinGame;
 using Microsoft.AspNetCore.Components;
 
@@ -6,13 +11,45 @@ namespace Melinoe.Client.Components
 {
     public partial class JoinGameDialogue
     {
-        public string GameCode { get; set; }
-        public string UserName { get; set; }
+        private class JoinGameModel
+        {
+            [Required]
+            [StringLength(maximumLength: 6, MinimumLength = 6, ErrorMessage = "Game code must be exactly 6 digits.")]
+            public string GameCode { get; set; } = string.Empty;
+
+            [Required]
+            [StringLength(maximumLength: 20, MinimumLength = 2, ErrorMessage = "Username must be between 4 and 20 alphanumeric characters.")]
+            public string UserName { get; set; } = string.Empty;
+        }
 
         [Inject]
         public IDispatcher Dispatcher { get; set; }
 
-        public void Connect() => 
-            Dispatcher.Dispatch(new JoinGameAction(GameCode, UserName));
+        private JoinGameModel Model { get; } = new();
+
+        private Validations _validations;
+
+        public async Task Connect()
+        {
+            if (!_validations.ValidateAll())
+                return;
+
+            _validations.ClearAll();
+            Dispatcher.Dispatch(new JoinGameAction(Model.GameCode, Model.UserName));
+        }
+
+        private static bool IsValidGameCode(string gameCode) => gameCode is not null && Regex.IsMatch(gameCode, @"^[0-9]{6}$");
+
+        private static void IsValidGameCode(ValidatorEventArgs args) =>
+            args.Status = IsValidGameCode(args.Value as string) 
+                ? ValidationStatus.Success 
+                : ValidationStatus.Error;
+
+        private static bool IsValidUserName(string userName) => userName is not null && Regex.IsMatch(userName, @"^[a-zA-Z0-9]{4,20}$");
+
+        private static void IsValidUserName(ValidatorEventArgs args) =>
+            args.Status = IsValidUserName(args.Value as string)
+                ? ValidationStatus.Success
+                : ValidationStatus.Error;
     }
 }
