27ef7a106a5021cbfaca17017a1e09333810aa1d
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-02-21 12:21:39 +0000

    Add theme colours.

--- Client/App.razor.cs
+++ Client/App.razor.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Blazorise;
+﻿using Blazorise;
 
 namespace Melinoe.Client
 {
     public partial class App
     {
-        private Theme Theme { get; } = new Theme();
+        private const string ColourPrimary = "#71BB00";
+        private const string ColourSecondary = "#00BBA8";
+        private const string ColourWarning = "#BBA800";
+        private const string ColourDanger = "#BB0014";
+
+        private Theme Theme { get; } = new()
+        {
+            BackgroundOptions = new ThemeBackgroundOptions(),
+            ColorOptions =
+                new ThemeColorOptions
+                {
+                    Primary = ColourPrimary,
+                    Secondary = ColourSecondary,
+                    Warning = ColourWarning,
+                    Danger = ColourDanger
+                },
+            TextColorOptions = new ThemeTextColorOptions
+            {
+                Primary = ColourPrimary,
+                Secondary = ColourSecondary,
+                Warning = ColourWarning,
+                Danger = ColourDanger
+            }
+        };
     }
 }
